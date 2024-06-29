5b4f7340536276d49559fcf5509d800c185941da
taeyang121096 <taeyang121096@naver.com>
2024-06-29 22:41:32 +0900

    jpa 기본

---
+++ jpa/src/test/java/nexon/study/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package nexon.study.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
