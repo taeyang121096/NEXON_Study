742c717ce5b049f340bb962858fe8eca185920de
taeyang121096 <taeyang121096@naver.com>
2024-12-14 16:25:04 +0900

    querydsl 설정 작업

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -4,31 +4,35 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QHello.*;
 
 @SpringBootTest
 @Transactional
-@Commit
 class QuerydslApplicationTests {
 
 	@PersistenceContext
 	EntityManager em;
+
+	JPAQueryFactory queryFactory;
+
+	@BeforeEach
+	void init() {
+		queryFactory = new JPAQueryFactory(em);
+	}
 
 	@Test
 	void contextLoads() {
 		Hello hello = new Hello();
 		em.persist(hello);
 
-		JPAQueryFactory query = new JPAQueryFactory(em);
-		QHello qHello = QHello.hello;
-
-		Hello result = query.selectFrom(qHello)
+		Hello result = queryFactory.selectFrom(QHello.hello)
 				.fetchOne();
 
 		assertThat(hello).isEqualTo(result);
